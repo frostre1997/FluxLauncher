cba9c31ac25a71ccb44dd12b4dedecfaa371ec41
Frostre_ <n9043395@gmail.com>
2026-06-27 11:58:48 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,5 @@
-
+dependencies {
+    implementation("androidx.compose.ui:ui:1.6.0")
+    implementation("androidx.compose.material3:material3:1.2.0")
+    implementation("androidx.activity:activity-compose:1.8.0")
+}
