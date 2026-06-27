4c6a3b9940b09f6dcf6c74941b40bb329c30f49a
Frostre_ <n9043395@gmail.com>
2026-06-27 11:55:51 +0200

    Update MainActivity.kt

--- app/src/main/java/com/flux/launcher/MainActivity.kt
+++ app/src/main/java/com/flux/launcher/MainActivity.kt
@@ -1,1 +1,25 @@
+package com.flux.launcher
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.flux.launcher.ui.theme.FluxLauncherTheme
+import com.flux.launcher.ui.components.AppGrid
+import com.flux.launcher.data.AppProvider
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        // Inizializziamo il nostro "cervello" che cerca le app
+        val appProvider = AppProvider(this)
+        appProvider.loadApps()
+        
+        setContent {
+            FluxLauncherTheme {
+                // Qui richiamiamo la griglia che abbiamo creato in AppGrid.kt
+                AppGrid(apps = appProvider.installedApps)
+            }
+        }
+    }
+}
