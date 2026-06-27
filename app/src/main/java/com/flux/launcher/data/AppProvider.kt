5f08b20c9ef170b2be5c53575521d6c7e5cea92c
Frostre_ <n9043395@gmail.com>
2026-06-27 11:46:35 +0200

    Update AppProvider.kt

--- app/src/main/java/com/flux/launcher/data/AppProvider.kt
+++ app/src/main/java/com/flux/launcher/data/AppProvider.kt
@@ -1,1 +1,35 @@
+package com.flux.launcher.data
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.LauncherApps
+import android.os.Process
+import androidx.compose.runtime.mutableStateListOf
+
+// Questa classe tiene traccia di tutte le app installate
+class AppProvider(private val context: Context) {
+    private val launcherApps = context.getSystemService(Context.LAUNCHER_APPS_SERVICE) as LauncherApps
+    
+    // Lista osservabile che si aggiornerà automaticamente nella UI
+    val installedApps = mutableStateListOf<AppInfo>()
+
+    fun loadApps() {
+        // Query per tutte le app installate nel profilo corrente
+        val apps = launcherApps.getActivityList(null, Process.myUserHandle())
+        
+        installedApps.clear()
+        apps.forEach { activityInfo ->
+            installedApps.add(AppInfo(
+                label = activityInfo.label.toString(),
+                packageName = activityInfo.applicationInfo.packageName,
+                icon = activityInfo.getIcon(0) // Recupera l'icona adattiva
+            ))
+        }
+    }
+}
+
+data class AppInfo(
+    val label: String,
+    val packageName: String,
+    val icon: android.graphics.drawable.Drawable
+)
